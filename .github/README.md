4e75def6aa5e3598aeb30a7687a4f816bc0f263d
stefan2008@gmail.com <stefanbeta2008@gmail.com>
2026-09-24 23:25:56 +0200

    Push

--- .github/README.md
+++ .github/README.md
@@ -59,6 +59,12 @@ Getting compiled binary from Git using `wget`
 wget https://raw.githubusercontent.com/Sbinator-hub/Haxefetch/main/binary/haxefetch && chmod +x haxefetch && sudo mv haxefetch /usr/bin/haxefetch
 ```
 
+Getting compiled binary from Git using `wget` (FreeBSD)
+```
+wget https://raw.githubusercontent.com/Sbinator-hub/Haxefetch/main/binary/haxefetch-bsd && chmod +x haxefetch-bsd && doas mv haxefetch-bsd /usr/bin/haxefetch
+```
+> - NOTE: FreeBSD does supports compiled binary that i am pushing from Ubuntu 22.04 for compability! You need to do this steps: 1. Enable Linux module `doas kldload linux && doas kldload linux64` -> 2. Install Linux base image `doas pkg linux_base-r19` -> 3. Enable Linux service `doas sysrc linux_enable="YES` -> 4. Start service `doas service linux start`
+
 </details>
 
 <details>
--- src/Haxefetch.hx
+++ src/Haxefetch.hx
@@ -65,7 +65,7 @@ class Haxefetch {
 
         #if (haxe_ver >= "4.3.0")
         var modules:Map<String, String> = [
-            "hostname" => Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, Colors.RED) : null,
+            "hostname" => Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, logoColor) : null,
             "host"     => (Configuration.showHost && host != null) ? (Configuration.showHost ? Colors.colorize(Configuration.hostString, logoColor) + Configuration.separator + separator + host : null) : null,
             "os"       => Configuration.showDistro ? Colors.colorize(Configuration.distroString, logoColor) + Configuration.separator + separator + distro + (Configuration.architecture ? archSuffix : "") + (Configuration.init ? initSuffix : "") : null,
             "kernel"   => Configuration.showKernel ? Colors.colorize(Configuration.kernelString, logoColor) + Configuration.separator + separator + (Configuration.systemKernel ? Configuration.systemKernelString + separator + kernel : kernel) : null,
@@ -86,7 +86,7 @@ class Haxefetch {
         ];
         #else
         var modules = new Map<String, String>();
-        modules.set("hostname", Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, Colors.RED) : null);
+        modules.set("hostname", Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, logoColor) : null);
         modules.set("host", (Configuration.showHost && host != null) ? (Configuration.showHost ? Colors.colorize(Configuration.hostString, logoColor) + Configuration.separator + separator + host : null) : null);
         modules.set("os", Configuration.showDistro ? Colors.colorize(Configuration.distroString, logoColor) + Configuration.separator + separator + distro + (Configuration.architecture ? archSuffix : "") + (Configuration.init ? initSuffix : "") : null);
         modules.set("kernel", Configuration.showKernel ? Colors.colorize(Configuration.kernelString, logoColor) + Configuration.separator + separator + (Configuration.systemKernel ? Configuration.systemKernelString + separator + kernel : kernel) : null);
