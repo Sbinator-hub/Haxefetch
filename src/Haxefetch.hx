7fd5f1629390f3a868865582a80b44cc3af2f2a1
stefan2008@gmail.com <stefanbeta2008@gmail.com>
2026-09-24 23:10:12 +0200

    Push

--- src/Haxefetch.hx
+++ src/Haxefetch.hx
@@ -63,8 +63,9 @@ class Haxefetch {
 
         var logo = Logo.fetchLogo(realDistro, Configuration.logoSize, Configuration.logo, logoColor, parseColor1, parseColor2);
 
+        #if (haxe_ver >= "4.3.0")
         var modules:Map<String, String> = [
-            "hostname" => Configuration.showHostname ? Colors.colorize(user, Colors.RED) + "@" + Colors.colorize(hostname, Colors.RED) : null,
+            "hostname" => Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, Colors.RED) : null,
             "host"     => (Configuration.showHost && host != null) ? (Configuration.showHost ? Colors.colorize(Configuration.hostString, logoColor) + Configuration.separator + separator + host : null) : null,
             "os"       => Configuration.showDistro ? Colors.colorize(Configuration.distroString, logoColor) + Configuration.separator + separator + distro + (Configuration.architecture ? archSuffix : "") + (Configuration.init ? initSuffix : "") : null,
             "kernel"   => Configuration.showKernel ? Colors.colorize(Configuration.kernelString, logoColor) + Configuration.separator + separator + (Configuration.systemKernel ? Configuration.systemKernelString + separator + kernel : kernel) : null,
@@ -83,6 +84,27 @@ class Haxefetch {
             "birth"    => Configuration.showBirth ? Colors.colorize(Configuration.birthString, logoColor) + Configuration.separator + separator + birth : null,
             "colors"   => Configuration.showBlock ? Colors.getColorBlocks() + (Configuration.showBlock2 ? separator + Colors.getBrightColorBlocks() : "") : null
         ];
+        #else
+        var modules = new Map<String, String>();
+        modules.set("hostname", Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, Colors.RED) : null);
+        modules.set("host", (Configuration.showHost && host != null) ? (Configuration.showHost ? Colors.colorize(Configuration.hostString, logoColor) + Configuration.separator + separator + host : null) : null);
+        modules.set("os", Configuration.showKernel ? Colors.colorize(Configuration.kernelString, logoColor) + Configuration.separator + separator + (Configuration.systemKernel ? Configuration.systemKernelString + separator + kernel : kernel) : null);
+        modules.set("kernel", Configuration.showKernel ? Colors.colorize(Configuration.kernelString, logoColor) + Configuration.separator + separator + (Configuration.systemKernel ? Configuration.systemKernelString + separator + kernel : kernel) : null);
+        modules.set("de", (desktop != null && desktop != "N/A" && desktop != "" && Configuration.showDesktop) ? Colors.colorize(Configuration.desktopString, logoColor) + Configuration.separator + separator + desktop : null);
+        modules.set("wm", (Configuration.showSession && session != null && session != "" && Sys.environment().get("DISPLAY") != null) ? Colors.colorize(Configuration.sessionString, logoColor) + Configuration.separator + separator + session + (Configuration.protocol ? ' (${protocol})' : '') : null);
+        modules.set("ram", Configuration.showRAM ? Colors.colorize(Configuration.ramString, logoColor) + Configuration.separator + separator + ram : null);
+        modules.set("swap", Configuration.showSWAP ? Colors.colorize(Configuration.swapString, logoColor) + Configuration.separator + separator + swap : null);
+        modules.set("cpu", Configuration.showCPU ? Colors.colorize(Configuration.cpuString, logoColor) + Configuration.separator + separator + cpu : null);
+        modules.set("gpu", Configuration.showGPU ? Colors.colorize(Configuration.gpuString, logoColor) + Configuration.separator +separator + gpu : null);
+        modules.set("disk", Configuration.showHostname ? Colors.colorize(user, logoColor) + "@" + Colors.colorize(hostname, Colors.RED) : null);
+        modules.set("packages", Configuration.showPackages ? Colors.colorize(Configuration.packageString, logoColor) + Configuration.separator + separator + packageSuffix : null);
+        modules.set("terminal", Configuration.showTerminal ? Colors.colorize(Configuration.terminalString, logoColor) + Configuration.separator + separator + terminal : null);
+        modules.set("shell", Configuration.showShell ? Colors.colorize(Configuration.shellString, logoColor) + Configuration.separator + separator + shell : null);
+        modules.set("uptime", Configuration.showUptime ? Colors.colorize(Configuration.uptimeString, logoColor) + Configuration.separator + separator + uptime : null);
+        modules.set("birthday", Configuration.showBirthday ? Colors.colorize(Configuration.birthdayString, logoColor) + Configuration.separator + separator + birthday : null);
+        modules.set("birth", Configuration.showBirth ? Colors.colorize(Configuration.birthString, logoColor) + Configuration.separator + separator + birth : null);
+        modules.set("colors", Configuration.showBlock ? Colors.getColorBlocks() + (Configuration.showBlock2 ? separator + Colors.getBrightColorBlocks() : "") : null);
+        #end
 
         var infoLine:Array<String> = Configuration.modules.map(function(key) return modules.get(key)).filter(function(line) return line != null);
 
